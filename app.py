437496dd260ce74e0c0eccc6b22c300e91eb1c0f
jaejoong991 <jeffrygaleon@gmail.com>
2025-07-25 10:47:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Hello from Flask!"
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
